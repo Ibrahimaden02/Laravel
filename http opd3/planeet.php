e2b74f05682f0d6c7938b297a952a049fa186c20
Ibrahimaden02 <133215513+Ibrahimaden02@users.noreply.github.com>
2024-09-23 13:30:02 +0200

    Delete http opd3 directory

--- http opd3/planeet.php
+++
@@ -1,9 +0,0 @@
-<?php
- 
- 
- 
-use Illuminate\Support\Facades\Route;
- 
-Route::get('/planeten', function () {
-    return ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
-});
